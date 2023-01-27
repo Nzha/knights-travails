a92fe25f488ad5d8db7bf5a2a1600deb466f0c48
Adrien <adrien.biz@gmail.com>
2023-01-27 21:41:07 +0400

    Boilerplate for BFS

--- knight-travails.js
+++ knight-travails.js
@@ -1,15 +1,11 @@
 const Node = (data) => {
   children = [];
-  return {
-    data,
-    children,
-  };
+  return { data, children };
 };
 
 const Knight = (coords = [0, 0]) => {
   const possibleMoves = (start = coords) => {
     const node = Node(start);
-
     const x = node.data[0];
     const y = node.data[1];
 
@@ -25,7 +21,6 @@ const Knight = (coords = [0, 0]) => {
     ];
 
     for (const combination of combinations) {
-      console.log(combination);
       if (combination[0] >= 0 && combination[1] >= 0)
         node.children.push(combination);
     }
@@ -33,12 +28,36 @@ const Knight = (coords = [0, 0]) => {
     return node;
   };
 
+  const levelOrder = (start, end) => {
+    const node = Node(start);
+    let result = [];
+    let queue = [];
+    queue.push(node);
+
+    // console.log(queue[0].data);
+    // console.log(node.data)
+    // console.log(end)
+
+    if (end.toString() === node.data.toString()) return result;
+
+    queue.push(possibleMoves(start));
+    console.log(queue);
+
+    // while (queue.length !== 0) {
+    //   let current = queue.shift();
+    //   result.push(current.data);
+
+    // }
+
+  };
+
   const moves = (start, end) => {};
 
-  return { coords, possibleMoves };
+  return { coords, possibleMoves, levelOrder };
 };
 
 const myKnight = Knight();
 
 console.log(myKnight.possibleMoves());
 console.log(myKnight.possibleMoves([3, 3]));
+console.log(myKnight.levelOrder([0, 0], [3, 3]));
